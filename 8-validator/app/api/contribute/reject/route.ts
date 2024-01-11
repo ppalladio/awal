86aeb8952e1011eaaf4e0defd4dfe97bc8a4afed
Yuxuan <nine.slide@gmail.com>
2024-01-11 18:43:30 +0100

    caching issue revalidating with force-dynamic

--- 8-validator/app/api/contribute/reject/route.ts
+++ 8-validator/app/api/contribute/reject/route.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export async function PATCH(req: Request, res: Response) {
     try {
         const body = await req.json();
